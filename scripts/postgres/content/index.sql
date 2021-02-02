3864e1e4d22b332c6c253d2e271c975f42b6100e
parihaaraka <parihaaraka@gmail.com>
2021-02-02 13:35:21 +0300

    comment on index

--- scripts/postgres/content/index.sql
+++ scripts/postgres/content/index.sql
@@ -1,7 +1,10 @@
 select E'DROP INDEX $schema.name$.$index.name$;\n\n' ||
-	regexp_replace(pg_get_indexdef($index.id$), '\m(on|using|with)\M', E'\n\t\\1', 'ig') || 
-	case 
-		when $index.tag$ = 0 then '' 
+	regexp_replace(pg_get_indexdef($index.id$), '\m(on|using|with)\M', E'\n\t\\1', 'ig') ||
+	case
+		when $index.tag$ = 0 then ''
 		else E'\n\tTABLESPACE ' || (pg_identify_object('pg_tablespace'::regclass::oid, $index.tag$, 0)).identity
 	end ||
-	E';\n' as script
+	E';\n\n' ||
+	format(E'COMMENT ON INDEX $schema.name$.$index.name$ IS %s;\n',
+			 coalesce(E'\n' || quote_literal(obj_description($index.id$, 'pg_class')), 'NULL'))
+	as script
