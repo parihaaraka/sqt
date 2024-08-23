d51664ac25791fde7a7e7adbb6f1fb7dc4eec3a5
parihaaraka <parihaaraka@gmail.com>
2024-08-23 13:34:43 +0300

    add comment to single selected column details (view column comment accessible)

--- scripts/postgres/content/column.sql
+++ scripts/postgres/content/column.sql
@@ -9,6 +9,8 @@ tmp2 as
 	select k,v
 	from jsonb_each_text(to_jsonb((select tmp from tmp)) - '{tablename,schemaname,attname}'::text[]) as x(k,v)
 )
-select coalesce(nullif(string_agg(format('%-25s: %s', k, v::text), E'\n'), ''), '-- statistics not found') as script
+select
+    coalesce(format(E'COMMENT ON COLUMN %I.%I.%I IS\n', '$schema.name$','$table.name$','$column.name$') || quote_literal(col_description($table.id$, $column.id$)) || E';\n\n', '') ||
+    coalesce(nullif(string_agg(format('%-25s: %s', k, v::text), E'\n'), ''), '-- statistics not found') as script
 from tmp2
 --order by 1
